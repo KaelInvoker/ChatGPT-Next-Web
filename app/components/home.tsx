5b440dc6f9049d7392171686ea62281c30d96bc2
kaer <kaer180@163.com>
2023-04-16 16:08:22 +0800

    1

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,12 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>智能助理</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            连接未来，从现在开始。
+          </div>
+          <div className={styles["desc"]}>
+            本站有全网最新最全chatGPT教程，内置上百条提示词条，输入/即可打开并支持搜索。如有问题，欢迎加我微信
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
@@ -203,11 +206,6 @@ function _Home() {
                 }}
                 shadow
               />
-            </div>
-            <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
-              </a>
             </div>
           </div>
           <div>
